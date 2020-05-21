bc541a8b9f8a5d3244fc320f34b78d088f3264df
piyush-mahapatra-au6 <piyushmahapatra001@gmail.com>
2020-05-22 03:20:21 +0530

    Stack Codes and a Basic Problem

--- DataStructure/Stack/stack.py
+++ DataStructure/Stack/stack.py
@@ -1,16 +1,3 @@
-# Stack Data Structure:
-- Stack is a linear data structure which follows a particular order in which the operations are performed. 
-- The order may be LIFO(Last In First Out) or FILO(First In Last Out).
-Ex: Stack of Books
-A
-B
-C
-D
-## Let's Code a Stack
-- Python’s buil-in data structure list can be used as a stack
-
-```python
-
 class Stack():
     def __init__(self):
         self.items =[]
@@ -18,10 +5,14 @@ class Stack():
         self.items.append(item)
     def pop(self):
         return self.items.pop()
-    def get_stack(self):
-        return self.items
     def isEmpty(self):
         return self.items ==[]
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[-1]
+    def get_stack(self):
+        return self.items
+
 
 s = Stack()
 # pushing A and B
@@ -34,5 +25,4 @@ print(s.get_stack())
 # popping,removing 
 s.pop()
 print(s.get_stack())
-
-```
+print(s.isEmpty()) #Check if stack is Empty
